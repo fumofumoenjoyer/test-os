578829cc2e108ba8ed54b5865383456a7144cfe5
Your Name <you@example.com>
2026-02-19 05:48:49 -0300

    fasf

---
+++ files/scripts/swap-kernel.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
+  rpm --erase $pkg --nodeps
+done
+
+pushd /usr/lib/kernel/install.d
+printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
+printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
+chmod +x  05-rpmostree.install 50-dracut.install
+popd
+
+dnf -y copr enable bieszczaders/kernel-cachyos-lto
+dnf -y copr disable bieszczaders/kernel-cachyos-lto
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-lto install \
+  kernel-cachyos-lto \
+  kernel-cachyos-lto-devel-matched
+
+dnf -y copr enable bieszczaders/kernel-cachyos-addons
+dnf -y copr disable bieszczaders/kernel-cachyos-addons
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons swap zram-generator-defaults cachyos-settings
+
+tee /usr/lib/modules-load.d/mizukios-ntsync.conf <<'EOF'
+ntsync
+EOF
